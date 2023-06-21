89000873c81a3a84c60496f53cdfe5039a4f636a
Adel.Khaled <adel.khaled@codecrafters-solutions.com>
2023-06-21 19:41:30 +0300

    [LCD Display]
    - LCD is working only

--- TopWayLCD_Interface.h
+++ TopWayLCD_Interface.h
@@ -8,6 +8,15 @@
 #ifndef TOPWAYLCD_INTERFACE_H_
 #define TOPWAYLCD_INTERFACE_H_
 
+#define In           0x00080000
+#define Out          0x00080002
+#define Validation   0x00000100
+#define Add          0x0008000A
+#define List         0x00080008
+#define Phone        0x00000080
+#define Calling_List 0x00000180
+#define SMS_List     0x00000B80
+#define Clear_BUFF   0x00080004
 
 void LCD_Init();
 
--- main.c
+++ main.c
@@ -22,19 +22,29 @@ int main(void)
 	 u8 *valid = "Valid Number";
 	 u8 *Invalid = "InValid Number";
 	 u8 *ptr = "";
-
+	 u8 valid_phone = 0;
+	 u8* data = "";
+	 u8 list;
+	 u8* calling_Array[20] = {"01063482890","01077777777","01000000000"};
+	 u8* sms_Array[20];
+	 static u8 calling_size = 0;
+	 u8 sms_size = 0;
+	 u8* p = "";
+	 u8* pp = "";
 	 while(1)
 	 {
 		 Num_size=0;
 		for(u8 i=0; i<=200; i++)
 		{
-			LCD_SendNum16(0x00080000,i);
-			LCD_SendNum16(0x00080002,200-i);
+			LCD_SendNum16(In,i);
+			LCD_SendNum16(Out,200-i);
 		}
 
-		u8* data = "";
-		LCD_GetString(0x00000080,data);
+		data = "";
+		LCD_GetString(0x00000000,data);
 		ptr = data;
+
+		/*Phone Number Validation*/
 		if(data[0] == '0' && data[1] == '1')
 		{
 			while(*data != '\0')
@@ -44,27 +54,66 @@ int main(void)
 			}
 			if(Num_size == 11)
 			{
-				LCD_SendNum16(0x00080004,Num_size);
-				LCD_SendString(0x00000100,valid);
-
-				Add_to_eeprom(ptr);
-//				print_numbers();
-//				UART_SendStringSync(ptr);
-				//u8 * mesg = "Hello CodeCrafters";
-				//UART_voidDisable();
-				//SIM_SendSMS(ptr,"Hello CodeCrafters");
-				LCD_SendString(0x00000080,"\0");
+				//LCD_SendNum16(0x00080004,Num_size);
+				LCD_SendString(Validation,valid);
+				valid_phone = 1;
 			}
 			else
 			{
-				LCD_SendString(0x00000100,Invalid);
+				LCD_SendString(Validation,Invalid);
+				LCD_SendNum16(Add,0);
 			}
-
 		}
 		else
 		{
-			LCD_SendString(0x00000100,Invalid);
+			LCD_SendString(Validation,Invalid);
+			LCD_SendNum16(Add,0);
 		}
+
+		/*Checking if user press ADD button and insert the number to its correct list*/
+		if(valid_phone && LCD_GetNum16(Add))
+		{
+			list = LCD_GetNum16(List);
+			if(list == 1)
+			{
+				calling_Array[calling_size] = ptr;
+				p = (u8*)calling_Array[calling_size];
+				LCD_SendString(Phone,p);
+				LCD_SendString(Calling_List + (128*calling_size), p);
+				LCD_SendNum16(0x0008000C,list);//test
+				calling_size++;
+			}else if(list == 2)
+			{
+				sms_Array[sms_size] = ptr;
+				p = (u8*)sms_Array[sms_size];
+				LCD_SendString(Phone,p);
+				p = (u8*)sms_Array[sms_size];
+				LCD_SendString(SMS_List + (128*sms_size) , p);
+				LCD_SendNum16(0x0008000C,list);//test
+				sms_size++;
+			}
+			LCD_SendNum16(Add,0);
+			LCD_SendNum16(Clear_BUFF,1);
+			valid_phone = 0;
+		}
+
+//		u8* t = (u8*)calling_Array[0];
+////		u8* tt = (u8*)calling_Array[1];
+////		u8* ttt = (u8*)calling_Array[2];
+//		LCD_SendString(Calling_List , t);
+//
+//		t = (u8*)calling_Array[1];
+//		LCD_SendString(Calling_List + 128, t);
+//
+//		t = (u8*)calling_Array[2];
+//		LCD_SendString(Calling_List +(128*2), t);
+
+//		for(u8 i =0 ; i<calling_size ; i++)
+//		{
+//			//u8* p = (u8*)calling_Array[i];
+//			LCD_SendString(Calling_List + (128*i) , p);
+//		}
+
 
 	 }
 
